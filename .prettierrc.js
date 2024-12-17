9b4142de3cd477a2f4b9b1971da7c0f945609e11
German <germanvershinin@yandex.ru>
2024-12-17 18:06:03 +0300

    prettier semi

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,23 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  singleQuote: true,
-  trailingComma: "es5",
-  arrowParens: "always",
-  printWidth: 120,
-  tabWidth: 4,
-  overrides: [
-    {
-      files: ["*.json"],
-      options: {
-        semi: true,
-        tabWidth: 2,
-      },
-    },
-  ],
+    singleQuote: true,
+    trailingComma: 'es5',
+    arrowParens: 'always',
+    printWidth: 120,
+    tabWidth: 4,
+    overrides: [
+        {
+            files: ['*.js'],
+            options: {
+                semi: true,
+                tabWidth: 2,
+            },
+        },
+        {
+            files: ['*.json'],
+            options: {
+                semi: false,
+            },
+        },
+    ],
 };
