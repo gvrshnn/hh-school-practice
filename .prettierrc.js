c631cd06a6cb06d4e78a6e0a46d58a6f53508d5b
German <germanvershinin@yandex.ru>
2024-12-22 21:43:03 +0300

    fix error

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,24 +1,17 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'es5',
-  arrowParens: 'always',
-  printWidth: 120,
-  tabWidth: 4,
-  insertFinalNewLine: 'true',
-  overrides: [
-    {
-      files: ['*.js'],
-      options: {
-        semi: true,
-        tabWidth: 2,
-      },
-    },
-    {
-      files: ['*.json'],
-      options: {
-        semi: false,
-      },
-    },
-  ],
+    singleQuote: true,
+    trailingComma: 'es5',
+    arrowParens: 'always',
+    printWidth: 120,
+    tabWidth: 4,
+    insertFinalNewLine: 'true',
+    overrides: [
+        {
+            files: ['*.json'],
+            options: {
+                semi: false,
+            },
+        },
+    ],
 };
